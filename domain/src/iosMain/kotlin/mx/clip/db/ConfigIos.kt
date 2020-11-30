854a669364a8ccd046e55cd1f3c9bd453eb81b8a
lauro <joselaurocastaneda@gmail.com>
2020-11-30 06:58:18 -0600

    Initial Config

--- domain/src/iosMain/kotlin/mx/clip/db/ConfigIos.kt
+++ domain/src/iosMain/kotlin/mx/clip/db/ConfigIos.kt
@@ -2,7 +2,7 @@ package mx.clip.db
 
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
-actual fun createDb():ClipDataBase?{
+actual fun createDB(): ClipDataBase?{
     val driver = NativeSqliteDriver(ClipDataBase.Schema, "clipdatabase.db")
     return ClipDataBase(driver = driver)
 }
